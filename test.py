e80de548cb2fdd5a12d30d91bf5a2a089c33d7da
yichenlin-emma <143434116+yichenlin-emma@users.noreply.github.com>
2024-04-06 17:11:44 -0400

    Fix plot with stim duration when delay happened

--- test.py
+++ test.py
@@ -184,9 +184,9 @@ print("The firing rate after stimulation is {} Hz.".format(firing_rate_after))
 
 # Reconstruct stim for plotting (add delay)
 epilep_stim_wave_plot = epilep_stim_wave
-epilep_stim_wave_plot[int(epilep_dur/h.dt):-1] = 0
+epilep_stim_wave_plot[int((epilep_dur+epilep_delay)/h.dt):-1] = 0
 e1_stim_wave_plot = e1_stim_wave
-e1_stim_wave_plot[int(e1_dur/h.dt):-1] = 0
+e1_stim_wave_plot[int((e1_dur+e1_delay)/h.dt):-1] = 0
 if epilep_delay > 0:
     epilep_stim_wave_plot[0:int(epilep_delay/h.dt)] = 0
 if e1_delay > 0:
